82be9fc6289f9eee9fa53c7c7df993776c443ed6
vaikhe2712 <65335234+vaikhe2712@users.noreply.github.com>
2022-11-17 12:37:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit
 
 streamlit.title('My parents New Healty Diner')
 streamlit.header('Menu')
-streamlit.print('bread'+'roti'+'\n brocoli')
+streamlit.print('bread'+'roti'+'\n'+ 'brocoli')
